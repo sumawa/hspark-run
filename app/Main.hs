b446fbb40092e8771cc0b3b91acdd225d2c44733
Sumant Awasthi <awasthi.sumant@gmail.com>
2021-01-08 10:28:47 +0530

    Struggling with haddock

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,20 @@
+-- | Main entry point for HSparkRun application
+
+{--
+-- | Read args from command line
+-- |   HSPARKRUN_ENV=dev HSPARKRUN_HOME=$(pwd) HSPARKRUN_TYPE=standalone
+-- |
+-- |     HSPARKRUN_TYPE : possible values (yarn/standalone) depending upon cluster manager for spark
+-- |    HSPARKRUN_ENV = dev / test / prod
+-- |    HSPARKRUN_HOME = $(pwd)    # current directory for spark run app
+-- |
+-- |   Workflow:
+-- |    * Read above mentioned arguments from command line and set the mode of execution
+-- |    * load spark run conf ( yarn or standalone configuration properties to reach cluster manager )
+-- |    * initialize source param (sql/file/... others)
+-- |    * Prepare RunData module
+-- |    * Run the module HSparkRunModule via the reader
+--}
 module Main where
 
 import StandaloneRun (StandaloneParam,readConf,evalConf)
@@ -12,6 +29,28 @@ import JobSource
 import System.Environment   
 import Data.List
 import Data.Typeable
+
+{-|
+ Read args from command line
+
+     HSPARKRUN_TYPE : possible values (yarn/standalone) depending upon cluster manager for spark
+
+     HSPARKRUN_ENV = dev / test / prod
+
+     HSPARKRUN_HOME = $(pwd)    # current directory for spark run app
+
+   Workflow:
+   
+    * Read above mentioned arguments from command line and set the mode of execution
+    
+    * load spark run conf ( yarn or standalone configuration properties to reach cluster manager )
+    
+    * initialize source param (sql/file/... others)
+    
+    * Prepare RunData module
+    
+    * Run the module HSparkRunModule via the reader
+-}
 
 -- FIXME: pick command line env with default values
 main :: IO ()
--- generate_doc.sh
+++ generate_doc.sh
@@ -1,1 +1,1 @@
-stack exec -- haddock --html app/Main.hs src/HSparkRunModule.hs src/models/Schema.hs src/source/JobSource.hs src/runner/SparkRun.hs src/runner/StandaloneRun.hs  --hyperlinked-source --odir=dist/docs
+stack exec -- haddock --html app/Main.hs src/HSparkRunModule.hs src/source/JobSource.hs src/runner/SparkRun.hs src/runner/StandaloneRun.hs  --hyperlinked-source --odir=dist/docs
